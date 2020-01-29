e75fed725a329f6b3400ce769418ab215c067b1d
DMdue <grrr754@naver.com>
2020-01-30 05:33:18 +0900

    init

---
+++ GithubSearch/GithubSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubSearch
+//
+//  Created by Jaycee on 2020/01/30.
+//  Copyright © 2020 Jaycee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
